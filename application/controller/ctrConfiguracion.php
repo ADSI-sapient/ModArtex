516e6561cdfac8fc51aed413eae9dc4a7cb15f05
Johan <jaac219@gmail.com>
2016-08-08 20:44:25 -0500

    Cambios ctrConfiguracion

--- application/controller/ctrConfiguracion.php
+++ application/controller/ctrConfiguracion.php
@@ -4,6 +4,8 @@ class ctrConfiguracion extends Controller{
 	private $_modelMedida;
 	private $_modelRoles;
 
+	public $mensaje = "";
+
 
 	function __construct(){
 		$this->_modelColor = $this->loadModel('mdlColores');
@@ -12,11 +14,18 @@ class ctrConfiguracion extends Controller{
 	}
 
 	public function index(){
+		var_dump("sgsf");
+		exit();
 		$lista = $this->_modelColor->listar();
 
 		include APP . 'view/_templates/header.php';
 		include APP . 'view/configuracion/colores.php';
 		include APP . 'view/_templates/footer.php';
+
+
+		if ($mensaje != "") {
+			echo("<script> swal('Hay cantidades asociadas a este registro', 'Regristro exitoso'); </script>");
+		}
 	}
 
 	public function listarColores(){
@@ -24,100 +33,66 @@ class ctrConfiguracion extends Controller{
 
 		include APP . 'view/_templates/header.php';
 		include APP . 'view/configuracion/colores.php';
-		include APP . 'view/_templates/footer.php';
-
-		if (isset($_GET["mes"])) {
-			if ($_GET["mes"] == 1) {
-				echo '<script type="text/javascript">
-					swal("Confirmación", "Se elimino correctamente el color", "success")
-					</script>';
-				}
-		if ($_GET["mes"] == 2) {
-				echo '<script type="text/javascript">
-						swal("Registro exitoso", "Se registro exitosamente el color", "success")
-					</script>';
-				}
-				if ($_GET["mes"] == 3) {
-					echo '<script type="text/javascript">
-						swal("Modificación exitosa", "Se modifico exitosamente el color", "success")
-					</script>';
-				}
-			}
-		}
+		include APP . 'view/_templates/footer.php';	
+	}
 
 	public function registrarColor(){
 		$this->_modelColor->__SET("_codigo", $_POST["codigo"]);
 		$this->_modelColor->__SET("_nombre", $_POST["nombre"]);
-		$this->_modelColor->registrar();
-
-			header ("location: ".URL."ctrConfiguracion/listarColores?mes=2");
+		if ($this->_modelColor->registrar()) {
+			$mensaje = "swal('Hay cantidades asociadas a este registro', 'Regristro exitoso')";
 		}
 
-		public function eliminarColor(){
-			$this->_modelColor->__SET("_id", $_GET["id"]);
-			$this->_modelColor->eliminar();
+		header ("location: ".URL."ctrConfiguracion/listarColores");
+	}
 
-			header("location: ".URL."ctrConfiguracion/listarColores?mes=1");
-		}
+	public function eliminarColor(){
+		$this->_modelColor->__SET("_id", $_GET["id"]);
+		$this->_modelColor->eliminar();
 
-		public function modificarColor(){
-			$this->_modelColor->__SET("_id", $_POST["id"]);
-			$this->_modelColor->__SET("_codigo", $_POST["codigo"]);
-			$this->_modelColor->__SET("_nombre", $_POST["nombre"]);
-			$this->_modelColor->modificar();
-			
-			header('location: '.URL.'ctrConfiguracion/listarColores?mes=3');
-		}
+		header("location: ".URL."ctrConfiguracion/listarColores");
+	}
 
-		public function listarMedidas(){
-			$lista = $this->_modelMedida->listar();
+	public function modificarColor(){
+		$this->_modelColor->__SET("_id", $_POST["id"]);
+		$this->_modelColor->__SET("_codigo", $_POST["codigo"]);
+		$this->_modelColor->__SET("_nombre", $_POST["nombre"]);
+		$this->_modelColor->modificar();
 
-			include APP . 'view/_templates/header.php';
-			include APP . 'view/configuracion/medidas.php';
-			include APP . 'view/_templates/footer.php'; 
+		header('location: '.URL.'ctrConfiguracion/listarColores');
+	}
 
-			if (isset($_GET["message"])) {
-				if ($_GET["message"] == 1) {
-					echo '<script type="text/javascript">
-						swal("Registro exitoso", "Se registro exitosamente la unidad de medida", "success")
-					</script>';
-				}
-				if ($_GET["message"] == 2) {
-					echo '<script type="text/javascript">
-						swal("Modificación exitosa", "Se modifico correctamente la unidad de medida", "success")
-					</script>';
-				}
-				if ($_GET["message"] == 3) {
-					echo '<script type="text/javascript">
-						swal("Confirmación", "Se elimino correctamente la unidad de medida", "success")
-					</script>';
-				}
-			}
-		}
+	public function listarMedidas(){
+		$lista = $this->_modelMedida->listar();
 
-		public function registrarMedida(){
-			$this->_modelMedida->__SET("_nombre", $_POST["nombre"]);
-			$this->_modelMedida->__SET("_abreviatura", $_POST["Abr"]);
-			$this->_modelMedida->registrarMedida();
+		include APP . 'view/_templates/header.php';
+		include APP . 'view/configuracion/medidas.php';
+		include APP . 'view/_templates/footer.php'; 
+	}
 
-			header('location: '.URL.'ctrConfiguracion/listarMedidas?message=1');
-		}
+	public function registrarMedida(){
+		$this->_modelMedida->__SET("_nombre", $_POST["nombre"]);
+		$this->_modelMedida->__SET("_abreviatura", $_POST["Abr"]);
+		$this->_modelMedida->registrarMedida();
 
-		public function eliminarMedida(){
-			$this->_modelMedida->__SET("_codigo", $_GET["cod"]);
-			$this->_modelMedida->eliminarMedida();
+		header('location: '.URL.'ctrConfiguracion/listarMedidas?message=1');
+	}
 
-			header('location: '.URL.'ctrConfiguracion/listarMedidas?message=3');
-		}
+	public function eliminarMedida(){
+		$this->_modelMedida->__SET("_codigo", $_GET["cod"]);
+		$this->_modelMedida->eliminarMedida();
 
-		public function modificarMedida(){
-			$this->_modelMedida->__SET("_codigo", $_POST["cod"]);
-			$this->_modelMedida->__SET("_nombre", $_POST["nombre"]);
-			$this->_modelMedida->__SET("_abreviatura", $_POST["abr"]);
-			$this->_modelMedida->modificarMedida();
+		header('location: '.URL.'ctrConfiguracion/listarMedidas?message=3');
+	}
 
-			header('location: '.URL.'ctrConfiguracion/listarMedidas?message=2');
-		}
+	public function modificarMedida(){
+		$this->_modelMedida->__SET("_codigo", $_POST["cod"]);
+		$this->_modelMedida->__SET("_nombre", $_POST["nombre"]);
+		$this->_modelMedida->__SET("_abreviatura", $_POST["abr"]);
+		$this->_modelMedida->modificarMedida();
+
+		header('location: '.URL.'ctrConfiguracion/listarMedidas?message=2');
+	}
 
 	public function RegistrarRoles(){
 
@@ -127,30 +102,30 @@ class ctrConfiguracion extends Controller{
 				$ultimoRol = $this->_modelRoles->ultimoRol()["rol"];
 				for ($i=0; $i < count($_POST["Idpermiso"]); $i++) { 
 
-				$this->_modelRoles->__SET("Id_Rol",$ultimoRol);
-				$this->_modelRoles->__SET("Id_Permiso", $_POST['Idpermiso'][$i]);
-				$this->_modelRoles->regPermisosAsociados();
+					$this->_modelRoles->__SET("Id_Rol",$ultimoRol);
+					$this->_modelRoles->__SET("Id_Permiso", $_POST['Idpermiso'][$i]);
+					$this->_modelRoles->regPermisosAsociados();
 				}
 			}else{
 			    // alert("Error al registrar");
-			 	}
-	        }
+			}
+		}
 
-	    $permisos = $this->_modelRoles->getAsoPermisos();
-	    $roles = $this->_modelRoles->getRoles();
-	        
-	    if (isset($_POST["btnModificarRol"])) {
-	      	if ($_POST["idRol"] != 1) {
+		$permisos = $this->_modelRoles->getAsoPermisos();
+		$roles = $this->_modelRoles->getRoles();
+
+		if (isset($_POST["btnModificarRol"])) {
+			if ($_POST["idRol"] != 1) {
 				$this->_modelRoles->__SET("Id_Rol", $_POST["idRol"] );
 				$this->_modelRoles->__SET("Nombre", $_POST["Nombre"] );
-	      		$this->_modelRoles->BorrarPermisos() && $this->_modelRoles->ModificarNombre();
+				$this->_modelRoles->BorrarPermisos() && $this->_modelRoles->ModificarNombre();
 
 				// echo json_encode($listas);
 				for ($i=0; $i < count($_POST["Idpermiso"]); $i++) { 
 					$this->_modelRoles->__SET("Id_Permiso", $_POST['Idpermiso'][$i]);
 					$this->_modelRoles->regPermisosAsociados();
 				}
-	      	}
+			}
 		}
 
 		include APP . 'view/_templates/header.php';
@@ -159,7 +134,7 @@ class ctrConfiguracion extends Controller{
 	}
 
 	public function listarR(){
-		
+
 		$this->_modelRoles->__SET("Id_Rol", $_POST["rol"]);
 		$listas = $this->_modelRoles->ListarPermisos();
 		echo json_encode($listas);	
@@ -167,15 +142,14 @@ class ctrConfiguracion extends Controller{
 
 	public function cambiarEstadoRol(){
 		$this->_modelRoles->__SET("Id_Rol", $_POST["Id_Rol"]);
-	    $this->_modelRoles->__SET("Estado", $_POST["Estado"]);
-	
+		$this->_modelRoles->__SET("Estado", $_POST["Estado"]);
+
 		$roles = $this->_modelRoles->cambiarEstadoRoles();
 
 		if ($roles) {
-		    echo json_encode(["v"=>1]);
+			echo json_encode(["v"=>1]);
 		}else{
-		    echo json_encode(["v"=>0]);
+			echo json_encode(["v"=>0]);
 		}
 	}
-
-	}
+}
